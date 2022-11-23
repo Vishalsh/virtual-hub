4fc24048f91a64cf67883ccb556b11e7877f2e54
Vishal Sharma <vishal.sharma2@bp.com>
2022-11-23 12:39:07 +0530

    Map with direction renderer. Currently breaking

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { GoogleMap } from "@react-google-maps/api";
 import { Marker } from "@react-google-maps/api";
 import { LocationInput } from "./LocationInput";
 
 import { DirectionsService } from "@react-google-maps/api";
+import { DirectionsRenderer } from "@react-google-maps/api";
 
 const containerStyle = {
   width: "800px",
@@ -11,8 +12,40 @@ const containerStyle = {
 };
 
 export const Map = ({ userLocation, locationPoints }) => {
-  console.log(locationPoints);
+  const [directions, setDirections] = useState(null);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    const waypoints = locationPoints.map((point) => ({
+      location: { lat: point.lat, lng: point.lng },
+      stopover: true,
+    }));
+
+    const origin = userLocation;
+    const destination = waypoints.pop()?.location;
+
+    const directionsService = new google.maps.DirectionsService();
+    directionsService.route(
+      {
+        origin: origin,
+        destination: destination,
+        travelMode: google.maps.TravelMode.DRIVING,
+        waypoints: waypoints,
+      },
+      (result, status) => {
+        console.log(result);
+        if (status === google.maps.DirectionsStatus.OK) {
+          setDirections(result);
+        } else {
+          setError(result);
+        }
+      }
+    );
+  });
+
+  if (error) {
+    return <h1>{error}</h1>;
+  }
   return (
     <GoogleMap
       mapContainerStyle={containerStyle}
@@ -20,6 +53,10 @@ export const Map = ({ userLocation, locationPoints }) => {
       zoom={10}
     >
       <Marker position={userLocation} />
+      <DirectionsRenderer
+        containerStyle={containerStyle}
+        directions={directions}
+      />
     </GoogleMap>
   );
 };
