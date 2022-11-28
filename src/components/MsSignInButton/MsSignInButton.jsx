dcca1dcbd37c69da412f3843b3d98bcf7be3b836
prashtw <prasht@thoughtworks.com>
2022-11-28 12:44:03 +0530

    add styling to login page

--- src/components/MsSignInButton/MsSignInButton.jsx
+++ src/components/MsSignInButton/MsSignInButton.jsx
@@ -1,6 +1,10 @@
 import {useMsal} from "@azure/msal-react";
 import styles from './MsSignInButton.module.scss'
 
+const MsLogo = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 23 23" width="18" height="18"><path fill="#f3f3f3" d="M0 0h23v23H0z"/><path fill="#f35325" d="M1 1h10v10H1z"/><path fill="#81bc06" d="M12 1h10v10H12z"/><path fill="#05a6f0" d="M1 12h10v10H1z"/><path fill="#ffba08" d="M12 12h10v10H12z"/></svg>
+)
+
 export const MsSignInButton = () => {
   const {instance} = useMsal();
 
@@ -12,7 +16,9 @@ export const MsSignInButton = () => {
 
   return (
     <button onClick={handleSignIn} className={styles.msSignInButton}>
-      <div className={styles.msSignInButton__icon}></div>
+      <div className={styles.msSignInButton__icon}>
+        <MsLogo />
+      </div>
       <span className={styles.msSignInButton__label}>Sign in with Microsoft</span>
     </button>
   )
