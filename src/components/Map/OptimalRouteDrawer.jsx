f7d593aa159161b99059fcc7966f5c0e8bf4e125
Vishal Sharma <vishal.sharma2@bp.com>
2022-11-29 13:19:13 +0530

    Fixing lint

--- src/components/Map/OptimalRouteDrawer.jsx
+++ src/components/Map/OptimalRouteDrawer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext} from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { DirectionsService, DirectionsRenderer } from '@react-google-maps/api';
 
 import * as http from '../../utils/http';
