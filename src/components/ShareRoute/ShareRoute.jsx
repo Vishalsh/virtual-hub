cb10c81239170640f85e6cec4893fe4cf05a1c79
prashtw <prasht@thoughtworks.com>
2022-11-29 18:45:58 +0530

    hide share button on desktop

--- src/components/ShareRoute/ShareRoute.jsx
+++ src/components/ShareRoute/ShareRoute.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShareNodes } from '@fortawesome/free-solid-svg-icons/faShareNodes';
 import { RWebShare } from 'react-web-share';
+import { MobileView } from 'react-device-detect';
 import styles from './ShareRoute.module.scss';
 
 export function ShareRoute({ routeSavedId, route, onPlanNewRoute }) {
@@ -19,33 +20,23 @@ export function ShareRoute({ routeSavedId, route, onPlanNewRoute }) {
     <div className={styles.shareRoute}>
       <div className={styles.shareRoute__wrapper}>
         <p>Route has been saved successfully</p>
-        {/* <MobileView> */}
-        {/*  <RWebShare */}
-        {/*    data={{ */}
-        {/*      text: `Here is your shortest route`, */}
-        {/*      url: `/route/some-route-id`, */}
-        {/*      title: "Shortest Route Finder", */}
-        {/*    }} */}
-        {/*    onClick={() => console.log("shared successfully!")} */}
-        {/*  > */}
-        {/*    <button>Share 🔗</button> */}
-        {/*  </RWebShare> */}
-        {/* </MobileView> */}
-        <RWebShare
-          data={{
-            text: shareMessageText,
-            url: `${window.location.origin}/route-planner/${routeSavedId}`,
-            title: 'Shortest Route Finder',
-          }}
-        >
-          <button type="button" className={styles.shareRoute__button}>
-            <FontAwesomeIcon
-              icon={faShareNodes}
-              className={styles.shareRoute__btnIcon}
-            />
-            <span>Share</span>
-          </button>
-        </RWebShare>
+        <MobileView>
+          <RWebShare
+            data={{
+              text: shareMessageText,
+              url: `${window.location.origin}/route-planner/${routeSavedId}`,
+              title: 'Shortest Route Finder',
+            }}
+          >
+            <button type="button" className={styles.shareRoute__button}>
+              <FontAwesomeIcon
+                icon={faShareNodes}
+                className={styles.shareRoute__btnIcon}
+              />
+              <span>Share</span>
+            </button>
+          </RWebShare>
+        </MobileView>
         <button type="button" onClick={planNewRoute} className={styles.shareRoute__link}>Plan a new Route</button>
       </div>
     </div>
