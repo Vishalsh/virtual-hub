88bc735437646209d95cfb485bda3320c3901d9a
prashtw <prasht@thoughtworks.com>
2022-11-29 20:09:58 +0530

    optimal route mobile implementation

--- src/components/OptimalRoute/OptimalRoute.jsx
+++ src/components/OptimalRoute/OptimalRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowDown } from '@fortawesome/free-solid-svg-icons/faArrowDown';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons/faArrowRight';
 import styles from './OptimalRoute.module.scss';
 
 const WAYPOINTS_LABELS = ['A', 'B', 'C', 'D'];
@@ -10,23 +11,42 @@ export function OptimalRoute({ route }) {
   return (
     <div className={styles.optimalRoute}>
       <h2 className={styles.optimalRoute__heading}>Optimal Route</h2>
-      {
-        optimalRoute.map((wayPoint, index) => (
-          <>
-            <div className={styles.optimalRoute__name}>
-              <span className={styles.optimalRoute__label}>{WAYPOINTS_LABELS[index]}</span>
-              <span className={styles.optimalRoute__text}>{wayPoint.name}</span>
-            </div>
-            {
-              index < optimalRoute.length - 1 && (
-              <span className={styles.optimalRoute__arrowDown}>
-                <FontAwesomeIcon icon={faArrowDown} />
-              </span>
-              )
-            }
-          </>
-        ))
-      }
+      <div className={styles.optimalRoute__row}>
+        {
+          optimalRoute.map((wayPoint, index) => (
+            <>
+              <span className={styles.optimalRoute__labelMobile}>{WAYPOINTS_LABELS[index]}</span>
+              <span className={styles.optimalRoute__textMobile}>{wayPoint.name}</span>
+              {
+                index < optimalRoute.length - 1 && (
+                  <span className={styles.optimalRoute__arrowRight}>
+                    <FontAwesomeIcon icon={faArrowRight} />
+                  </span>
+                )
+              }
+            </>
+          ))
+        }
+      </div>
+      <div className={styles.optimalRoute__list}>
+        {
+          optimalRoute.map((wayPoint, index) => (
+            <>
+              <div className={styles.optimalRoute__name}>
+                <span className={styles.optimalRoute__label}>{WAYPOINTS_LABELS[index]}</span>
+                <span className={styles.optimalRoute__text}>{wayPoint.name}</span>
+              </div>
+              {
+                index < optimalRoute.length - 1 && (
+                  <span className={styles.optimalRoute__arrowDown}>
+                    <FontAwesomeIcon icon={faArrowDown} />
+                  </span>
+                )
+              }
+            </>
+          ))
+        }
+      </div>
     </div>
   );
 }
